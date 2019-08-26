6b7b521b22c41c4d70d2ea83fc7b2d5e8969ee7c
sagininuel <sagininuel@gmail.com>
2019-08-26 16:20:49 +0300

    deploy to heroku update

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,14 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
 /* eslint-disable no-console */
-import express from 'express';
-import bodyParser from 'body-parser';
-import apiRoutes from './routes/index';
-import errorHandler from './middlewares/errorHandler';
-import swaggerDoc from './swaggerDocs';
+import express from "express";
+import bodyParser from "body-parser";
+import apiRoutes from "./routes/index";
+import errorHandler from "./middlewares/errorHandler";
+import swaggerDoc from "./swaggerDocs";
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import 'babel-polyfill';
+import "babel-polyfill";
 
 const app = express();
 const port = 3000;
@@ -14,14 +16,17 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: false,
-  }),
+    extended: false
+  })
 );
 
 swaggerDoc(app);
 
-app.use('/api', apiRoutes);
-app.use('*', (req, res) => res.status(404).json({ status: 404, error: 'Page Not Found' }),);
+app.use("/api", apiRoutes);
+app.use("*", (req, res) => res.status(404).json({
+    status: 404,
+    error: "Page Not Found"
+  }));
 
 app.use(errorHandler.sendError);
 app.listen(port, () => console.log(`listening on port ${port}!`));
